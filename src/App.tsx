9ceeb683f817ea171e16a1c6ab21038067f9c904
WellyaKardika <wellyakardika13@gmail.com>
2026-07-08 17:26:18 +0800

    add Lenis library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
+import Lenis from 'lenis';
 import Home from './pages/Home';
 import ProjectDetail from './pages/ProjectDetail';
 
@@ -8,33 +9,53 @@ const App: React.FC = () => {
 
   // Scroll to top or section on route change
   useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
     if (location.pathname === '/') {
       const scrollTarget = sessionStorage.getItem('scrollTo');
       if (scrollTarget) {
         sessionStorage.removeItem('scrollTo');
         setTimeout(() => {
           const element = document.getElementById(scrollTarget);
-          if (element) element.scrollIntoView({ behavior: 'smooth' });
+          if (element) lenis.scrollTo(element, { offset: -80 });
         }, 100);
       }
     } else if (location.pathname === '/project') {
       setTimeout(() => {
         const element = document.getElementById('projects');
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth' });
-        }
+        if (element) lenis.scrollTo(element, { offset: -80 });
       }, 100);
     } else if (location.hash) {
       setTimeout(() => {
         const id = location.hash.replace('#', '');
         const element = document.getElementById(id);
-        if (element) {
-          element.scrollIntoView({ behavior: 'smooth' });
-        }
+        if (element) lenis.scrollTo(element, { offset: -80 });
       }, 100);
     } else {
-      window.scrollTo(0, 0);
+      lenis.scrollTo(0, { immediate: true });
     }
+
+    return () => {
+      lenis.destroy();
+    };
   }, [location.pathname, location.hash]);
 
   return (
